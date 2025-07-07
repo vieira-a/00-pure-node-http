263edf2016c3f6ffc270563fa4a6a63d88a82c9e
Anderson Vieira <asvieira.dev@gmail.com>
2025-07-07 15:24:30 -0300

    feat: setup environment schema validation with zod

---
+++ src/configs/env-config.ts
@@ -0,0 +1,26 @@
+import { config } from 'dotenv';
+import { z } from 'zod';
+
+config();
+
+const envSchema = z.object({
+  PORT: z.string().regex(/^\d+$/, 'PORT must be a valid number.'),
+  DATABASE_PG_URL: z
+    .string()
+    .url()
+    .refine((val) => val.startsWith('postgres://'), {
+      message: 'DATABASE_PG_URL must start with "postgres://"',
+    }),
+});
+
+const parsedEnv = envSchema.safeParse(process.env);
+
+if (!parsedEnv.success) {
+  console.error('Environment variables validation error', parsedEnv.error.format());
+  process.exit(1);
+}
+
+export const envConfig = {
+  PORT: Number(parsedEnv.data.PORT),
+  DATABASE_PG_URL: String(parsedEnv.data.DATABASE_PG_URL),
+};
