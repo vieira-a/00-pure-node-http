15d0687add8eda33ccd7db8dd0d2d5046be3a4df
Anderson Vieira <asvieira.dev@gmail.com>
2025-07-09 19:16:04 -0300

    feat: change api version to api prefix

--- src/configs/env-config.ts
+++ src/configs/env-config.ts
@@ -12,7 +12,7 @@ const envSchema = z.object({
       message: 'DATABASE_PG_URL must start with "postgres://"',
     }),
   NODE_ENV: z.enum(['development', 'production', 'test']),
-  API_VERSION: z.string(),
+  API_PREFIX: z.string(),
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
@@ -26,5 +26,5 @@ export const envConfig = {
   PORT: Number(parsedEnv.data.PORT),
   DATABASE_PG_URL: String(parsedEnv.data.DATABASE_PG_URL),
   NODE_ENV: String(parsedEnv.data.NODE_ENV),
-  API_VERSION: String(parsedEnv.data.API_VERSION),
+  API_PREFIX: String(parsedEnv.data.API_PREFIX),
 };
